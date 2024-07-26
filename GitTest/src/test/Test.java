81a062432837d664e2e6d6bb1407046b2964b1b7
user1 <user1@DESKTOP-VBII80A>
2024-07-26 17:01:31 +0900

    초기화

--- GitTest/src/test/Test.java
+++ GitTest/src/test/Test.java
@@ -1,5 +1,11 @@
 package test;
 
 public class Test {
+	public static void main(String[] args) {
+		
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+	}
 
 }
